2b037ca9f2d146c35432aaeebc5775ddd5550066
Wachirapong Prasertwong <ni@oozou.com>
2021-10-21 15:13:13 +0700

    add app color

---
+++ lib/src/core/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColor {
+  static const riptide = Color(0xFF86E3CE);
+  static const caper = Color(0xFFD0E6A5);
+  static const salomie = Color(0xFFFFDD94);
+  static const geraldine = Color(0xFFFA897B);
+  static const lightWisteria = Color(0xFFCCABD8);
+}
